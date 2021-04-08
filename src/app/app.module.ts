8c6fe9c6d5c69e8a00099e089155b80bfd14b4dc
hugofpaiva <hugofpaiva@ua.pt>
2021-04-08 22:19:14 +0100

    Tab, colors and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { RouterModule } from '@angular/router';
 import { ToastrModule } from "ngx-toastr";
 
 import { SidebarModule } from './sidebar/sidebar.module';
-import { FooterModule } from './shared/footer/footer.module';
 import { NavbarModule} from './shared/navbar/navbar.module';
-import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
 
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ExcessiveSpeedComponent } from './pages/excessive-speed/excessive-speed.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    ExcessiveSpeedComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -27,8 +27,6 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     SidebarModule,
     NavbarModule,
     ToastrModule.forRoot(),
-    FooterModule,
-    FixedPluginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/excessive-speed/excessive-speed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excessive-speed',
+  templateUrl: './excessive-speed.component.html',
+  styleUrls: ['./excessive-speed.component.css']
+})
+export class ExcessiveSpeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
